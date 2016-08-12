416039302f970aefab16bb8b4f59341552c50ab6
JamesEarle <j_earle@hotmail.com>
2016-08-12 14:53:48 -0700

    initial setup, including release transformation for Web.config to use Azure SQL Server

---
+++ TwitterSentiments/Models/Request.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Web;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+
+namespace TwitterSentiments.Models
+{
+    public class Request
+    {
+        // Primary key
+        public int ID { get; set; }
+
+        // Form inputs
+        [Display(Name = "Twitter Handle")]
+        public string TwitterHandle { get; set; }
+        public string Count { get; set; }
+
+        // Auto timestamp
+        [Display(Name = "Date Created")]
+        public DateTime Timestamp { get; } = DateTime.Now;
+    }
+
+    public class RequestDbContext : DbContext
+    {
+        public DbSet<Request> Requests { get; set; }
+    }
+}
---
+++ TwitterSentiments/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TwitterSentiments.Startup))]
+namespace TwitterSentiments
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
