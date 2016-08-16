d2ba20e7b45958dba2644d6b26a372dbaf757abb
JamesEarle <j_earle@hotmail.com>
2016-08-16 15:30:50 -0700

    UI changes with descriptions and image

--- TwitterSentiments/Controllers/RequestsController.cs
+++ TwitterSentiments/Controllers/RequestsController.cs
@@ -1,10 +1,8 @@
-﻿using System.Data.Entity;
-using System.Linq;
+﻿using System.Linq;
 using System.Net;
 using System.Web.Mvc;
 using TwitterSentiments.Models;
 using TwitterSentiments.App_Start;
-using System.Diagnostics;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
 using System;
@@ -86,37 +84,6 @@ namespace TwitterSentiments.Controllers
                 return RedirectToAction("Index");
             }
 
-            return View(request);
-        }
-
-        // GET: Requests/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Request request = db.Requests.Find(id);
-            if (request == null)
-            {
-                return HttpNotFound();
-            }
-            return View(request);
-        }
-
-        // POST: Requests/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,TwitterHandle,Count,Result")] Request request)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(request).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
             return View(request);
         }
 
